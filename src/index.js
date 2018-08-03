282aa180d9babd8df8f6e90c5f2f518eecc2d046
Your Name <you@example.com>
2018-08-03 10:30:15 +0800

    React-Toter 初期導入

--- src/index.js
+++ src/index.js
@@ -1,17 +1,70 @@
 import 'babel-polyfill';
-import React from 'react';
+import React, {PropTypes} from 'react';
 import { render } from 'react-dom';
+import { Router, Route, IndexRoute, browserHistory, Link, IndexLink } from 'react-router';
+
+const Header = () => {
+  return (
+    <nav className="nav nav--center">
+      <IndexLink to="/">Home</IndexLink>
+      {" | "}
+      <Link to="/a">A</Link>
+      {" | "}
+      <Link to="/b">B</Link>
+      {" | "}
+      <Link to="/c">C</Link>
+    </nav>
+  );
+};
+const Home = () => {
+  return (
+    <h2>hello Home</h2>
+  );
+};
+const A_Page = () => {
+  return (
+    <h2>hello A_Page</h2>
+  );
+};
+const B_Page = () => {
+  return (
+    <h2>hello B_Page</h2>
+  );
+};
+const C_Page = () => {
+  return (
+    <h2>hello C_Page</h2>
+  );
+};
 
 
 class App extends React.Component {
   render() {
     return (
-      <div>Hello React 123</div>
+      <div>
+        <Header/>
+         {this.props.children}
+      </div>
     );
   }
 }
+App.propTypes = {
+  children: PropTypes.object.isRequired
+};
+
+const routes =  (
+  <Route path="/" component={App}>
+    <IndexRoute component={Home} />
+    <Route path="a" component={A_Page} />
+    <Route path="b" component={B_Page} />
+    <Route path="c" component={C_Page} />
+  </Route>
+);
 
 render(
-    <App/>,
+  <Router
+    history={browserHistory}
+    routes={routes}
+  />,
   document.getElementById('app')
 );
