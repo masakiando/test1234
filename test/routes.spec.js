2eff91fd463a8c4e98e9219341af49edc309e83f
Your Name <you@example.com>
2018-08-21 10:21:21 +0800

    a

--- test/routes.spec.js
+++ test/routes.spec.js
@@ -10,6 +10,45 @@ var should = chai.should();
 
 chai.use(chaiHttp);
 
+describe('API Routes', function() {
+
+describe('Method that does not exist', function() {
+  it('Responding to an error', function(done) {
+    chai.request(server)
+    .options('/api/startkit')//req.method not method:options
+    .end(function(err, res) {
+      res.should.have.status(404);
+      res.should.be.json;
+      done();
+    });
+  });
+});
+
+describe('Get all data', function() {
+  it('should get all data', function(done) {
+    chai.request(server)
+    .get('/api/startkit')
+    .end(function(err, res) {
+      res.should.have.status(200);
+      res.should.be.json;
+      res.body.should.be.a('object');
+      res.body.status.should.equal('success');
+      res.body.message.should.equal('Retrieved ALL data');
+      res.body.data.length.should.equal(4);
+      res.body.data[0].should.have.property('name');
+      res.body.data[0].name.should.equal('Suits');
+      res.body.data[0].should.have.property('channel');
+      res.body.data[0].channel.should.equal('USA Network');
+      res.body.data[0].should.have.property('genre');
+      res.body.data[0].genre.should.equal('Drama');
+      res.body.data[0].should.have.property('rating');
+      res.body.data[0].rating.should.equal(3);
+      res.body.data[0].should.have.property('explicit');
+      res.body.data[0].explicit.should.equal(false);
+      done();
+    });
+  });
+});
   describe('Get all shows', function() {
     it('should get all shows', function(done) {
       chai.request(server)
@@ -19,7 +58,7 @@ chai.use(chaiHttp);
         res.should.be.json;
         res.body.should.be.a('object');
         res.body.status.should.equal('success');
-        res.body.message.should.equal('Retrieved ALL stertkit');
+        res.body.message.should.equal('Retrieved ALL data');
         res.body.data.length.should.equal(4);
         res.body.data[0].should.have.property('name');
         res.body.data[0].name.should.equal('Suits');
@@ -57,6 +96,20 @@ chai.use(chaiHttp);
         res.body.data.rating.should.equal(3);
         res.body.data.should.have.property('explicit');
         res.body.data.explicit.should.equal(false);
+        done();
+      });
+    });
+    it('We guarantee that the ID parameter exists from the query string',
+      function (done) {
+      chai.request(server)
+      .get('/api/startkit/2019')
+      .end(function (err, res) {
+        res.should.have.status(400);
+        res.should.be.json;
+        res.body.should.be.a('object');
+        res.body.status.should.equal('error');
+        res.body.message.should.equal(`ID '2019' does not exist.`);
+        res.body.error.should.equal(2019);
         done();
       });
     });
@@ -104,11 +157,9 @@ describe('POST /api/startkit', function() {
       explicit: true
     })
     .end(function (err, res) {
-      console.log(res.body.data);
       res.should.have.status(200);
       res.should.be.json;
       res.body.should.be.a('object');
-      console.log(res.body.data);
       res.body.data.should.have.property('name');
       res.body.data.name.should.equal('Suits');
       res.body.data.should.have.property('channel');
@@ -126,17 +177,15 @@ describe('POST /api/startkit', function() {
     chai.request(server)
     .put('/api/startkit/1')
     .send({
-      id: 20,
+      id: 20000,
       rating: 8,
       explicit: true
     })
     .end(function(err, res) {
       res.should.have.status(422);
       res.should.be.json; // jshint ignore:line
-      res.body.should.be.a('object');
-      res.body.should.have.property('error');
-      res.body.error.should.equal('You cannot update the id field');
       done();
     });
   });
+});
 });
--- tools/startkit.js
+++ tools/startkit.js
@@ -1,16 +1,195 @@
 var express =  require('express');
 var colors =  require('colors');
 var isEmpty =  require('lodash/isEmpty');
+var Validator =  require('Validator');
+
 var Startkit = require('./startkitModel');
 
 var router = express.Router();
-
 console.log('starting stertkitServer...'.white);
 
+function validPostObjectKey(req, res, next) {
+  var payload = req.body;
+  var keys = ['name', 'channel', 'genre', 'rating', 'explicit'];
+  var errors = [];
+  keys.forEach(function(key){
+    if(!(key in payload)) {
+      errors.push(key);
+    }
+  });
+  if( !(isEmpty(errors)) ){
+    res.status(400).json({
+      status: 'error',
+      message: `Invalid JSON. '${errors}' does not exist.`,
+      error: errors
+    });
+  } else {
+    return next();
+  }
+}
 
-// return ALL stertkit
-router.get('/', function(req, res, next) {
-  // res.send('send shows back');//red test用
+function validPostObjectType(req, res, next) {
+  var { name, channel, genre, rating, explicit } = req.body;
+  var errors = {};
+  var message = '入力が必須です。';
+  if (!name) errors.name = message;
+  if (!channel) errors.channel = message;
+  if (!genre) errors.genre = message;
+  if (!rating) errors.rating = message;
+  if (!explicit) errors.explicit = message;
+  if( !isEmpty(errors) ) {
+    res.status(400).json(errors);
+  } else {
+    return next();
+  }
+}
+
+function checkIDisInteger(req, res, next) {
+  var pupID = req.params.id;
+  if (isNaN(pupID)) {
+    res.status(400).json({
+      status: 'error',
+      message: req.params.id+':'+'It can not contain anything other than an integer'
+    });
+  } else {
+    return next();
+  }
+}
+// We guarantee that the ID parameter exists from the query string
+function checkID(req, res, next) {
+  var pupID = parseInt(req.params.id);
+    Startkit.query({
+      where: { id: req.params.id }
+    })
+    .fetch()
+    .then(function(data) {
+      if(!data) {
+        /* jshint ignore:start */
+        res.status(400)
+          .json({
+            status: 'error',
+            message: `ID '${pupID}' does not exist.`,
+            error: pupID
+          });
+        /* jshint ignore:end */
+      } else {
+        return next();
+      }
+    })
+    .catch(function(err) {
+      return next(err);
+    });
+}
+
+//Ensure that JSON objects are valid for PUT requests
+function validPutObjectKey(req, res, next) {
+    var obj = req.body;
+    var array = [];
+    Object.keys(obj).forEach((key)=> {
+       if (!(/id|channel|explicit|genre|name|rating/.test(key))) {
+         array.push(key);
+       }
+       if(key ==='id') {
+         return res.status(422).json({
+           status: 'error',
+           message: 'key No Rows Updated.',
+           error: key
+         });
+       }
+    });
+    if( !(isEmpty(array)) ){
+      res.status(400).json({
+        status: 'error',
+        message: array+' '+'key Unknown KEY',
+        error: array
+      });
+    } else {
+      return next();
+    }
+}
+function validPutObjectType(req, res, next) {
+  var { name, channel, genre, rating, explicit } = req.body;
+  var obj = req.body;
+  var errors = {};
+  var message = '入力が必須です。';
+  for (var key in obj) {
+    if(key==="name"){
+      if (!obj[key]) {
+        errors.name = message;
+      }
+    }
+    if(key==="channel"){
+      if (!obj[key]) {
+        errors.channel = message;
+      }
+    }
+    if(key==="genre"){
+      if (!obj[key]) {
+        errors.genre = message;
+      }
+    }
+    if(key==="rating"){
+      if ( isNaN(obj[key]) ) {
+        errors.rating = '数字のみを入力してください';
+      }
+    }
+    if(key==="explicit"){
+      if ( !( /false|true/.test(obj[key]) ) ) {
+      console.log('hihihihihihihi');
+        errors.explicit = 'The :attr field must be true or false.';
+      }
+    }
+  }
+  if( !isEmpty(errors) ) {
+    res.status(400).json(errors);
+  } else {
+    return next();
+  }
+}
+// API ROOT
+//  Get flow
+router.get(
+  '/',//API ROOT
+  get,//REQ.METHOD TO FUNCTION
+  errorsHandling//TO DE.HANDLING
+);
+// Get Single flow
+router.get(
+  '/:id',//API ROOT,
+  checkIDisInteger,//HELPER FUNCTION
+  checkID,//HELPER FUNCTION
+  getSingle,//REQ.METHOD TO FUNCTION
+  errorsHandling//TO DE.HANDLING
+);
+// Post flow
+router.post(
+  '/',//API ROOT
+  validPostObjectKey,//Validator
+  validPostObjectType,//Validator
+  post,//REQ.METHOD TO FUNCTION
+  errorsHandling//TO DE.HANDLING
+);
+// Post flow
+router.put(
+  '/:id',//API ROOT
+  checkIDisInteger,//HELPER FUNCTION
+  checkID,//HELPER FUNCTION
+  validPutObjectKey,//Validator
+  validPutObjectType,//Validator
+  put,//REQ.METHOD TO FUNCTION
+  errorsHandling//TO DE.HANDLING
+);
+// Delete flow
+router.delete(
+  '/:id', //API ROOT
+  checkIDisInteger,//HELPER FUNCTION
+  checkID,//HELPER FUNCTION
+  deletE,//REQ.METHOD TO FUNCTION
+  errorsHandling//TO DE.HANDLING
+);
+
+// get METHOD FUNCTION
+function get(req, res, next) {
   Startkit.collection()
   .fetch()
   .then(function(data) {
@@ -18,36 +197,28 @@ router.get('/', function(req, res, next) {
     json({
       data: data,
       status: 'success',
-      message: 'Retrieved ALL stertkit'
+      message: 'Retrieved ALL data'
     });
   })
   .catch(function(err) {
     return next(err);
   });
-});
+}
 
-// *** GET Single *** //
-//masakiando$ http -v get localhost:3000/api/startkit/dfd
-router.get('/:id',function (req, res, next) {
+// get single METHOD FUNCTION
+function getSingle(req, res, next) {
   var stertkitID = req.params.id;
   Startkit.query({
     where: { id: req.params.id }
   })
   .fetch()
   .then(function (data) {
-    if(data) {
-      res.status(200)
-      .json({
-        status: 'success',
-        data: data,
-        message: 'Retrieved ONE stertkit'
-      });
-    } else {
-        res.status(400).json({
-          status: 'error',
-          message: 'ID:'+req.params.id+' '+'does not exist.'
-        });
-    }
+    res.status(200)
+    .json({
+      status: 'success',
+      data: data,
+      message: 'Retrieved ONE stertkit'
+    });
   })
   .catch(function (err) {
     if (err.code === "22P02") {
@@ -57,123 +228,111 @@ router.get('/:id',function (req, res, next) {
         error: err
       });
     } else {
-      res.status(500).json({
-        error: err
-      });
+      return next(err);//errorsHandling
     }
   });
-});
-
-// insert startkit
-router.post('/', function (req, res, next) {
-  Startkit.forge({//カラムの値が空でも保存されちゃう
-    id: req.params.id,
-    channel: req.body.channel,
-    explicit: req.body.explicit,
-    genre: req.body.genre,
-    name: req.body.name,
-    rating: req.body.rating
-  }).save()
-    .then(function(data) {
-      res.status(200).json({
-        status: 'success',
-        data: data,
-        message: 'Inserted one startkit'
-      });
-    })
-    .catch(function(err) {//get database error res fun 作る全部で利用
-      if (err.code === "23505") {//unique error
-        res.status(400).json({
-          status: 'error',
-          message: req.body.name+' '+'not_unique.',//再利用できるメッセーッジに変える
-          error: err
-        });
-      } else if(err.code === "23502"){//column null error
-        res.status(400).json({
-          status: 'error',
-          message: err.column+'_column does not exist.'+'(非NULL違反)',
-          error: err
-        });
-      } else {
-        res.status(500).json({
-          error: err
-        });
-      }
-    });
-});
-
-function validPutObject(req, res, next) {
-    var obj = req.body;
-    var array = [];
-    Object.keys(obj).forEach((key)=> {
-        if (/id|channel|explicit|genre|name|rating/.test(key)) {
-        } else {
-         array.push(key);
-        }
-    });
-    if( !(isEmpty(array)) ){
-      res.status(400).json({
-        status: 'error',
-        message: array+' '+'key は　何???',
-        error: array
-      });
-    } else {
-      return next();
-    }
 }
-// *** put show *** //
-router.put('/:id', validPutObject, function(req, res, next) {
+
+// post METHOD FUNCTION
+function post(req, res, next) {
+    var { channel, explicit, genre, name, rating } = req.body;
+      Startkit.forge(
+        { channel, explicit, genre, name, rating })
+      .save()
+        .then(function(data) {
+          res.status(200).json({
+            status: 'success',
+            data: data,
+            message: 'Inserted one startkit'
+          });
+        })
+        .catch(function(err) {
+          if (err.code === "23505") {//unique error
+            res.status(400).json({
+              status: 'error',
+              message: req.body.name+' '+'not_unique.',
+              error: err
+            });
+          } else if(err.code === "23502"){//column null error
+            res.status(400).json({
+              status: 'error',
+              message: err.column+'_column does not exist.'+'(非NULL違反)',
+              error: err
+            });
+          } else {
+            return next(err);//errorsHandling
+          }
+        });
+}
+
+// put METHOD FUNCTION
+function put(req, res, next) {
   Startkit.query({
     where: { id: req.params.id }
   })
   .fetch()
   .then(function (data) {
     data.save(req.body)
-  .then(function(a) {
-    res.status(200).json({
-      status: 'success',
-      data: a,
-      message: 'put ONE stertkit'
+    .then(function (a) {
+      res.status(200).json({
+        status: 'success',
+        data: a,
+        message: 'put one data'
+      });
+    })
+    .catch(function (err) {
+      return next(err);//errorsHandling
     });
-  })
-  .catch(function(err) {
+  });
+}
+
+// delete METHOD FUNCTION
+function deletE(req, res, next) {
+
+    Startkit.query({
+      where: { id: req.params.id }
+    })
+    .fetch()
+    .then(function (data) {
+      console.log(data);
+      if(data) {
+        data.destroy()
+        .then(function (a) {
+          res.status(200).json({
+            status: 'success',
+            data: a,
+            message: 'delete ONE stertkit'
+          });
+        });
+      } else {
+          res.status(400).json({
+            status: 'error',
+            message: 'ID:'+req.params.id+' '+'does not exist.'
+          });
+      }
+    })
+    .catch(function (err) {
+        return next(err);//errorsHandling
+    });
+}
+
+// helper functions//
+//Handling common database errors
+function errorsHandling(err, req, res, next) {
+ if(err.code === "42P01"){//not Table
+  res.status(400).json({
+    status: 'error',
+    message: 'parserOpenTable',
+    error: err
+  });
+  } else {
     res.status(500).json({
       error: err
     });
-  });
-});
-});
+  }
+}
 
-router.delete('/:id', function (req, res, next) {
-  Startkit.query({
-    where: { id: req.params.id }
-  })
-  .fetch()
-  .then(function (data) {
-    console.log(data);
-    if(data) {
-      data.destroy()
-      .then(function (a) {
-        res.status(200).json({
-          status: 'success',
-          data: a,
-          message: 'delete ONE stertkit'
-        });
-      });
-    } else {
-        res.status(400).json({
-          status: 'error',
-          message: 'ID:'+req.params.id+' '+'does not exist.'
-        });
-    }
-  })
-  .catch(function (err) {
-    res.status(500).json({error: err});
-  });
-});
-
-
-//req.method get post put delete not
+// req.method get post put delete not
 router.use((req, res) => {
   res.status(404).json({
     errors: {
