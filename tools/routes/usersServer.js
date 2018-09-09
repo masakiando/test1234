e19e07521b22c72ab89051b6e1d5bfe2f5f5881c
Your Name <you@example.com>
2018-09-09 07:36:39 +0700

    a

--- tools/routes/usersServer.js
+++ tools/routes/usersServer.js
@@ -2,3 +2,14 @@ var express =  require('express');
 var colors =  require('colors');
 var isEmpty =  require('lodash/isEmpty');
 var Validator =  require('Validator');
+
+var router = express.Router();
+
+router.post(
+  '/',//API ROOT
+  function (req, res) {
+     console.log(req.body);
+  }
+);
+
+module.exports = router;
