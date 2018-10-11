1ae1dc2b817bc67c88cd75dcb454c63e07ddb673
Your Name <you@example.com>
2018-10-11 10:08:40 +0700

    m

--- db/migrations/20181011092238_connect_attribute_key_and_attribute_value.js
+++ db/migrations/20181011092238_connect_attribute_key_and_attribute_value.js
@@ -1,11 +1,12 @@
 
 exports.up = function(knex, Promise) {
-return knex.schema.createTable('connect_attribute_key_and_attribute_value', function(table){
-table.increments('id').index().unsigned().primary();
-table.integer('attribute_key_id').index().unsigned().notNullable().references('id').inTable('attribute_keys').onDelete('CASCADE');
-table.integer('attribute_values_id').index().unsigned().notNullable().references('id').inTable('attribute_values').onDelete('CASCADE');
-});
+  return knex.schema.createTable('connect_attribute_key_and_attribute_value', function(table){
+    table.increments('id').index().unsigned().primary();
+    table.integer('attribute_key_id').index().unsigned().notNullable().references('id').inTable('attribute_keys').onDelete('CASCADE');
+    table.integer('attribute_values_id').index().unsigned().notNullable().references('id').inTable('attribute_values').onDelete('CASCADE');
+  });
 };
 
 exports.down = function(knex, Promise) {
-return knex.schema.dropTable('connect_attribute_key_and_attribute_value');
+  return knex.schema.dropTable('connect_attribute_key_and_attribute_value');
+};
