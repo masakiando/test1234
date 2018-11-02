e0bef2df5267785ed3ff09514615dbbd40746ac7
Your Name <you@example.com>
2018-11-02 13:45:06 +0700

    rxjsでなくsageに変更

--- src/store/configureStore.dev.js
+++ src/store/configureStore.dev.js
@@ -2,18 +2,22 @@ import {createStore, applyMiddleware, compose } from 'redux';
 import rootReducer from '../reducers';//index
 import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
 import thunk from 'redux-thunk';
-
+import createSagaMiddleware from "redux-saga";
+// import rootSaga from "../sagas";
 function configureStore(initialState) {
+  const sagaMiddleware = createSagaMiddleware();
   return createStore(
     rootReducer,
     initialState,
     compose(
       applyMiddleware(
         thunk,
+        sagaMiddleware,
         reduxImmutableStateInvariant()
       ),
       window.devToolsExtension ? window.devToolsExtension() : f => f
     )
   );
+  // sagaMiddleware.run(rootSaga);
 }
 export default configureStore;
--- src/store/configureStore.prod.js
+++ src/store/configureStore.prod.js
@@ -1,17 +1,21 @@
 import {createStore, applyMiddleware, compose} from 'redux';
 import rootReducer from '../reducers';
 import thunk from 'redux-thunk';
-
+import createSagaMiddleware from "redux-saga";
+// import rootSaga from "../sagas";
 function configureStore(initialState) {
+  const sagaMiddleware = createSagaMiddleware();
   return createStore(
     rootReducer,
     initialState,
     compose(
       applyMiddleware(
-        thunk
+        thunk,
+        sagaMiddleware
       ),
       window.devToolsExtension ? window.devToolsExtension() : f => f
     )
   );
+  // sagaMiddleware.run(rootSaga);
 }
 export default configureStore;
